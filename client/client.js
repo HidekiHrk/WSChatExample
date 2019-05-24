7cd27f7469d318e9f409ad2bb73ce584ba73ec0c
HidekiHrk <hidekihiroki123@gmail.com>
2019-05-24 04:29:46 -0300

    .

---
+++ client/client.js
@@ -0,0 +1,38 @@
+var is_connected = false;
+var text_input = document.getElementById('text_input');
+
+var ws = new WebSocket("ws://0.0.0.0:8000/");
+
+ws.addEventListener('open', () => {
+    is_connected = true;
+    ws.send(JSON.stringify({type:"connect"}));
+})
+
+ws.addEventListener('message', (msg) => {
+    let message = JSON.parse(msg.data);
+    if(message.type == "update"){
+        let chat_block = document.getElementById('chat_block');
+        chat_block.innerHTML = message.data.text;
+        chat_block.scrollTop = chat_block.scrollHeight;
+    }
+});
+
+ws.addEventListener('error', (err) => {
+    document.getElementById('chat_block').innerHTML = 
+        "Ocorreu um erro na conexão com o websocket, por favor recarregue a página.";
+})
+
+text_input.addEventListener('keydown', function(e){
+    if(e.keyCode == 13){
+        send_message();
+    }
+})
+
+function send_message() {
+    let msg = document.getElementById('text_input').value;
+    let name = document.getElementById('name_input').value;
+    if(!is_connected || !msg.length) return;
+    document.getElementById('text_input').value = "";
+    name = name.length > 0 ? name : 'Guest';
+    ws.send(JSON.stringify({type:"message", data:{name: name, text: msg }}));
+}
---
+++ main.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+var wss = new WebSocket.Server({port:8000});
+
+var chat_file_path = "./chat.txt"
+
+function add_message(txt){
+    let text = fs.readFileSync(chat_file_path).toString(encoding="utf-8");
+    text += `${txt}<br>`;
+    fs.writeFileSync(chat_file_path, text, {encoding:'utf-8'});
+}
+
+function read_text(){
+    return fs.readFileSync(chat_file_path).toString(encoding="utf-8");
+}
+
+wss.on('connection', ws => {
+    ws.on('message', msg => {
+        let message = JSON.parse(msg.toString());
+        console.log(message);
+        if(!message.type){
+            ws.send("error");
+        }
+        switch(message.type){
+            case "connect":
+                ws.send(JSON.stringify({type:"update", data:{text:read_text()}}));
+                break;
+            case "message":
+                add_message(`${message.data.name}: ${message.data.text}`);
+                ws.send(JSON.stringify({type:"update", data:{text:read_text()}}));
+                break;
+        }
+    });
+});
